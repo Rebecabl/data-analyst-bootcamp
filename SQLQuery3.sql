46bbaf968eb5ecdec9dbdefa52c382e3e4116f12
Rebeca Barbosa <107273033+Rebecabl@users.noreply.github.com>
2023-12-30 20:05:43 -0300

    Where Statement

---
+++ SQLQuery3.sql
@@ -0,0 +1,45 @@
+/*
+where Statement
+=, <>, <, >, AND, OR, LIKE, NULL, NOT NULL, IN
+*/
+
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE FirstName <> 'Jim'
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE Age <= 32
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE Age <= 32 AND Gender = 'Male'
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE Age <= 32 OR Gender = 'Male'
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE LastName LIKE 'S%'
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE LastName LIKE 'S%o%'
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE LastName LIKE 'S%ott%'
+
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE FirstName is NOT NULL
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE FirstName IN ('Jim', 'Michael')
+
+
+
