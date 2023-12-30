6ed50d806f1470f91be69da9bdb903b9f191aec7
Rebeca Barbosa <107273033+Rebecabl@users.noreply.github.com>
2023-12-30 20:19:40 -0300

    Group By + Order By Statements

---
+++ SQLQuery4.sql
@@ -0,0 +1,44 @@
+/*
+Group By, Order By
+*/
+
+--SELECT *
+--FROM EmployeeDemographics
+
+--SELECT *
+--FROM EmployeeDemographics
+
+--SELECT Gender, COUNT(Gender)
+--FROM EmployeeDemographics
+--GROUP BY Gender
+
+--SELECT Gender, Age, COUNT(Gender)
+--FROM EmployeeDemographics
+--GROUP BY Gender
+
+--SELECT Gender, COUNT(Gender)
+--FROM EmployeeDemographics
+--WHERE Age > 31
+--GROUP BY Gender
+
+--SELECT Gender, COUNT(Gender) AS CountGender
+--FROM EmployeeDemographics
+--WHERE Age > 31
+--GROUP BY Gender
+--ORDER BY Gender DESC
+
+--SELECT *
+--FROM EmployeeDemographics
+--ORDER BY Age DESC, Gender DESC
+
+--SELECT *
+--FROM EmployeeDemographics
+--ORDER BY 1,2,3,4,5
+
+--SELECT *
+--FROM EmployeeDemographics
+--ORDER BY 4 Desc, 5 Desc
+
+
+
+
