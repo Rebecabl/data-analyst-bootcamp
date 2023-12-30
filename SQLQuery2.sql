24b5e6164c82011a71a18e7cd89aee709b7a6e58
Rebeca Barbosa <107273033+Rebecabl@users.noreply.github.com>
2023-12-30 19:52:15 -0300

    Select + From Statements

---
+++ SQLQuery2.sql
@@ -0,0 +1,20 @@
+--SELECT *
+--FROM EmployeeDemographics
+
+--SELECT FirstName, LastName
+--FROM EmployeeDemographics
+
+--SELECT TOP 5*
+--FROM EmployeeDemographics
+
+--SELECT DISTINCT(Gender)
+--FROM EmployeeDemographics
+
+--SELECT COUNT(LastName) AS LastNameCount
+--FROM EmployeeDemographics
+
+--SELECT MAX(Salary)
+--FROM EmployeeSalary
+
+--SELECT *
+--FROM SQLTutorial.dbo.EmployeeSalary
